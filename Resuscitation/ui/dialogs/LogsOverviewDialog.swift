44729630875731dd128c00cf9514a202d5921d61
dankito <git@dankito.net>
2020-10-23 22:26:53 +0200

    Implemented deleting logs

--- Resuscitation/ui/dialogs/LogsOverviewDialog.swift
+++ Resuscitation/ui/dialogs/LogsOverviewDialog.swift
@@ -18,7 +18,7 @@ struct LogsOverviewDialog: View {
     var body: some View {
         Form {
             Section {
-                List(logs) { log in
+                ForEach(logs) { log in
                     NavigationLink(destination: LazyView(LogDialog(log, presenter))) {
                         HStack {
                             Text(presenter.formatDate(log.startTime))
@@ -29,9 +29,28 @@ struct LogsOverviewDialog: View {
                         }
                     }
                 }
+                .onDelete(perform: delete)
             }
         }
         .navigationBarTitle("Logs", displayMode: .inline)
+        .navigationBarItems(trailing: EditButton())
+    }
+    
+    
+    private func delete(at offsets: IndexSet) {
+        for offset in offsets {
+            let log = logs[offset]
+            askUserToDeleteLog(log)
+        }
+    }
+    
+    private func askUserToDeleteLog(_ log: ResuscitationLogInfo) {
+        UIAlert(
+            "Really delete log '%@'?".localize(presenter.formatDate(log.startTime) + " " + presenter.formatTime(log.startTime)),
+            "All data for this account will be permanently deleted locally.",
+            UIAlertAction.destructive("Delete") { presenter.deleteResuscitationLog(log) },
+            UIAlertAction.cancel()
+        ).show()
     }
 
 }
